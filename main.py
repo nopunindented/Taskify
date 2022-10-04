0cf88433ef1a72e449ae537028b2b6d9120e7220
Muhammad Umer Fiaz <111707985+nopunindented@users.noreply.github.com>
2022-10-04 04:07:23 +0000

    "Tested discord.py library and now integrating bot commands"

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 import discord
+from discord import commands
+from discord import guilds
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+
+import os
+from dotenv import load_dotenv
+intents = discord.Intents.default()
+intents.message_content = True
+
+load_dotenv()
+TOKEN= os.getenv('DISCORD_TOKEN')
+client= commands.Bot()
+
+@client.comman
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  #to prevent bot from replying to itself
+  if message.author== client.user:
+    return
+
+    
+  elif message.content== '$hello':
+    await message.channel.send('bruh')
+
+
+client.run(TOKEN)
